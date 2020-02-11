5833c6ecf3a284d2b89f3e4a462c9820ff61ba4e
Rob Waight <43173714+rwaight@users.noreply.github.com>
2020-02-11 16:41:34 -0600

    Update Start_DokuWikiStick.ps1
    Add link to DokuWiki on a Stick installation page

--- Start_DokuWikiStick.ps1
+++ Start_DokuWikiStick.ps1
@@ -1,4 +1,5 @@
 # Used to start DokuWiki on a Stick and log the output
+# https://www.dokuwiki.org/install:dokuwiki_on_a_stick
 cd C:\path\to\dokuwiki\directory\DokuWikiStick
 Sleep 2
 Start-Transcript run_DokuWiki_$(Get-Date -format 'yyyyMMdd-HHmmss').log
